f17bb913cf7b658c4eb7d3ffaa17c0a3af165d76
Aidan63 <aidan.lee63@gmail.com>
2022-12-03 20:10:54 +0000

    [DbgEng] Remove un-needed process events

--- hxcppdbg-core/src/hxcppdbg/core/drivers/dbgeng/native/DebugEventCallbacks.cpp
+++ hxcppdbg-core/src/hxcppdbg/core/drivers/dbgeng/native/DebugEventCallbacks.cpp
@@ -22,9 +22,7 @@ HRESULT hxcppdbg::core::drivers::dbgeng::native::DebugEventCallbacks::GetInteres
 {
     *mask =
         DEBUG_EVENT_BREAKPOINT |
-        DEBUG_EVENT_EXCEPTION |
-        DEBUG_EVENT_CREATE_PROCESS |
-        DEBUG_EVENT_EXIT_PROCESS;
+        DEBUG_EVENT_EXCEPTION;
     return S_OK;
 }
 
@@ -36,25 +34,4 @@ HRESULT hxcppdbg::core::drivers::dbgeng::native::DebugEventCallbacks::Breakpoint
 HRESULT hxcppdbg::core::drivers::dbgeng::native::DebugEventCallbacks::Exception(PEXCEPTION_RECORD64, ULONG firstChance)
 {
     return DEBUG_STATUS_BREAK;
-}
-
-HRESULT hxcppdbg::core::drivers::dbgeng::native::DebugEventCallbacks::CreateProcess(
-    _In_ ULONG64 imageFileHandle,
-    _In_ ULONG64 handle,
-    _In_ ULONG64 baseOffset,
-    _In_ ULONG moduleSize,
-    _In_ PCWSTR moduleName,
-    _In_ PCWSTR imageName,
-    _In_ ULONG checkSum,
-    _In_ ULONG timeDateStamp,
-    _In_ ULONG64 initialThreadHandle,
-    _In_ ULONG64 threadDataOffset,
-    _In_ ULONG64 startOffset)
-{
-    return DEBUG_STATUS_NO_CHANGE;
-}
-
-HRESULT hxcppdbg::core::drivers::dbgeng::native::DebugEventCallbacks::ExitProcess(ULONG ExitCode)
-{
-    return DEBUG_STATUS_NO_CHANGE;
 }
--- hxcppdbg-core/src/hxcppdbg/core/drivers/dbgeng/native/DebugEventCallbacks.hpp
+++ hxcppdbg-core/src/hxcppdbg/core/drivers/dbgeng/native/DebugEventCallbacks.hpp
@@ -16,18 +16,5 @@ namespace hxcppdbg::core::drivers::dbgeng::native
 
         STDMETHOD(Breakpoint)(_In_ PDEBUG_BREAKPOINT2 bp);
         STDMETHOD(Exception)(_In_ PEXCEPTION_RECORD64 exception, _In_ ULONG firstChance);
-        STDMETHOD(CreateProcess)(
-            _In_ ULONG64 imageFileHandle,
-            _In_ ULONG64 handle,
-            _In_ ULONG64 baseOffset,
-            _In_ ULONG moduleSize,
-            _In_ PCWSTR moduleName,
-            _In_ PCWSTR imageName,
-            _In_ ULONG checkSum,
-            _In_ ULONG timeDateStamp,
-            _In_ ULONG64 initialThreadHandle,
-            _In_ ULONG64 threadDataOffset,
-            _In_ ULONG64 startOffset);
-        STDMETHOD(ExitProcess)(_In_ ULONG ExitCode);
     };
 }
